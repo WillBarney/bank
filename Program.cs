5fe03ac9e9e6d4acc538acbcfea0f8e3b9b457bf
WillBarney <86446498+WillBarney@users.noreply.github.com>
2021-12-16 11:30:47 -0500

    added interest simulation/main menu overhaul
    The menu is now based off regular expressions and using if/else statements. With the regular expressions we are able to make more in-depth functions. The simulate will use a customer id, account id and the years to simulate for. The simulate function will first validate the existence of the customer, the account and also make sure the balance on the account is greater than 0. It will then perform the compound interest calculation and show the user the results.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace Bank
         {
             new Customer(976080,"joe","smith","979456123","123 joe rd","june")
         };
+        //list to keep up with employees
         private static List<Employee> employees = new List<Employee>
         {
             new Employee(123456,"will","barney","manager","april")
@@ -21,82 +23,153 @@ namespace Bank
         //random number generator
         private static Random rnd = new Random();
 
+        //expressions
+        private static List<Regex> regexes = new List<Regex>
+        {
+            new Regex("^(create)[ ](customer)$"), //index 0
+            new Regex("^(create)[ ](account)$"), //index 1
+            new Regex("^(create)[ ](deposit)$"), //index 2
+            new Regex("^(create)[ ](withdraw)$"), //index 3
+            new Regex("^(quit)$"), //index 4
+            new Regex("^(show)[ ](customers)$"), //index 5
+            new Regex("^(show)[ ](accounts)$"), //index 6
+            new Regex("^(test)$"), //index 7
+            new Regex("^(simulate)[ ][0-9]{6}[ ][0-9]{8}[ ][0-9]{1,2}$") //index 8 - simulate word, then a 6 digit customerID, then a 8 digit accountID, then a number of years to calculate for
+        };
+
         // login and loop bools
         private static bool toContinue = true;
         private static bool adminLoggedIn = false;
 
         public static void Main(string[] args)
         {
+            //AdminLogIn();
+            MainMenu();
+        }
+        
+        public static void MainMenu()
+        {
             while(toContinue)
             {
                 Console.Write("Enter a function: ");
                 string function = Console.ReadLine();
 
-                switch(function)
-                {   
-                    //create customer case: confirm admin login, call create customer method
-                    case "create customer":
-                        //check if an admin has logged in
-                        if(!adminLoggedIn)
-                        {
-                            //admin login
-                            AdminLogIn();
-                            //check if admin logged in successfully
-                            if(toContinue)
-                            {
-                                CreateCustomer();
-                            }
-                            else
-                            {
-                                break;    
-                            }
-                        }
-                        //check if admin already logged in
-                        else if(adminLoggedIn)
+                if(regexes[0].IsMatch(function)) //create customer validation
+                {
+                    if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
                         {
                             CreateCustomer();
-                            break;
                         }
-                        break;
-                    case "create account":
-                        //check if an admin has logged in
-                        if(!adminLoggedIn)
-                        {
-                            //admin login
-                            AdminLogIn();
-                            //check if admin logged in successfully
-                            if(toContinue)
-                            {
-                                CreateAccount();
-                            }
-                            else
-                            {
-                                break;    
-                            }
-                        }
-                        //check if admin already logged in
-                        else if(adminLoggedIn)
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        CreateCustomer();
+                    }
+                }
+                else if(regexes[1].IsMatch(function)) //create account validation
+                {
+                    if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
                         {
                             CreateAccount();
-                            break;
                         }
-                        break;
-                    case "show customers":
-                        ShowCustomers();
-                        break;
-                    case "show accounts":
-                        ShowAccounts();
-                        break;
-                    case "create deposit":
-                        Deposit();
-                        break;
-                    case "create withdraw":
-                        Withdraw();
-                        break;
-                    case "quit":
-                        toContinue = false;
-                        break;
+                        
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        CreateAccount();
+                    }    
                 }
+                else if(regexes[2].IsMatch(function)) //create deposit validation
+                {
+                    if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
+                        {
+                            Deposit();
+                        }
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        Deposit();
+                    }
+                }
+                else if(regexes[3].IsMatch(function)) //create withdraw validation
+                {
+                    if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
+                        {
+                            Withdraw();
+                        }
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        Withdraw();
+                    }
+                }
+                else if(regexes[4].IsMatch(function)) //quit validation
+                {
+                    toContinue = false;
+                }
+                else if(regexes[5].IsMatch(function)) //show customers validation
+                {
+                     if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
+                        {
+                            ShowCustomers();
+                        }
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        ShowCustomers();
+                    }
+                }
+                else if(regexes[6].IsMatch(function)) //show accounts validation
+                {
+                     if(!adminLoggedIn)
+                    {
+                        //admin login
+                        AdminLogIn();
+                        //check if admin logged in succesfully
+                        if(toContinue)
+                        {
+                            ShowAccounts();
+                        }
+                    }
+                    else if(adminLoggedIn)
+                    {
+                        ShowAccounts();
+                    }
+                }
+                else if(regexes[7].IsMatch(function)) //test validation
+                {
+                    ExpressionTest();
+                }
+                else if(regexes[8].IsMatch(function))
+                {
+                    Simulate(function);
+                }
+
             }
         }
 
@@ -241,6 +314,10 @@ namespace Bank
                     accNum = 01;
                     joinedNumber = int.Parse(cusID.ToString()+"0"+accNum.ToString());
                     break;
+                case "money market":
+                    accNum = 02;
+                    joinedNumber = int.Parse(cusID.ToString()+"0"+accNum.ToString());
+                    break;
                 default:
                     accNum = 11;
                     joinedNumber = int.Parse(cusID.ToString()+"0"+accNum.ToString());
@@ -250,6 +327,7 @@ namespace Bank
             return joinedNumber;
         }
 
+        //deposit: ask for customer id(validated) then ask for account id(validated). When the deposit amount is given it must be greater than 0. The deposit amount is added to the selected account.
         private static void Deposit()
         {
             Account matchedAccount;
@@ -267,35 +345,42 @@ namespace Bank
                 chosenCustomer = customers.Where(x => x.customerID == cusID).ToList();
             }
 
-            Console.Write("enter account id: ");
-            int accID = int.Parse(Console.ReadLine());
-
-            var chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
-
-            while(chosenAccount.Count() != 1)
+            if(chosenCustomer.First().accounts.Count() == 0)
             {
-                Console.Write("account does not exist. enter another account id: ");
-                accID = int.Parse(Console.ReadLine());
-
-                chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+                Console.WriteLine("this customer has no accounts");
             }
-
-            matchedAccount = chosenAccount.First();
-
-            Console.Write("enter deposit amount: ");
-            decimal deposit = decimal.Parse(Console.ReadLine());
-
-            while(deposit <= 0.0m)
+            else
             {
-                Console.Write("deposit must be greater than 0.00. enter another deposit amount: ");
-                deposit = decimal.Parse(Console.ReadLine());
-            }
+                Console.Write("enter account id: ");
+                int accID = int.Parse(Console.ReadLine());
 
-            matchedAccount.balance += deposit;
-            Console.WriteLine($"{deposit:C} added to account {matchedAccount.accountNumber}");
+                var chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+
+                while(chosenAccount.Count() != 1)
+                {
+                    Console.Write("account does not exist. enter another account id: ");
+                    accID = int.Parse(Console.ReadLine());
+
+                    chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+                }
+
+                matchedAccount = chosenAccount.First();
+
+                Console.Write("enter deposit amount: ");
+                decimal deposit = decimal.Parse(Console.ReadLine());
+
+                while(deposit <= 0.0m)
+                {
+                    Console.Write("deposit must be greater than 0.00. enter another deposit amount: ");
+                    deposit = decimal.Parse(Console.ReadLine());
+                }
+
+                matchedAccount.balance += deposit;
+                Console.WriteLine($"{deposit:C} added to account {matchedAccount.accountNumber}");
+            }
         }
 
-        
+        //withdraw: ask for customerID(validation) then ask for accountID(validation). When the withdraw amount is given it must be greater than 0 and less than the current balance
         private static void Withdraw()
         {
             Account matchedAccount;
@@ -313,33 +398,40 @@ namespace Bank
                 chosenCustomer = customers.Where(x => x.customerID == cusID).ToList();
             }
 
-            Console.Write("enter account id: ");
-            int accID = int.Parse(Console.ReadLine());
-
-            var chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
-
-            while(chosenAccount.Count() != 1)
+            if(chosenCustomer.First().accounts.Count() == 0)
             {
-                Console.Write("account does not exist. enter another account id: ");
-                accID = int.Parse(Console.ReadLine());
-
-                chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+                Console.WriteLine("this customer has no accounts");
             }
-
-            matchedAccount = chosenAccount.First();
-            decimal currentBalance = matchedAccount.balance;
-
-            Console.Write("enter withdrawal amount: ");
-            decimal withdrawal = decimal.Parse(Console.ReadLine());
-
-            while(withdrawal <= 0.0m || withdrawal >= (currentBalance * 0.5m))
+            else
             {
-                Console.Write("withdrawal must be greater than 0.00\nand can not be more than half your current balance.\nenter another withdrawal amount: ");
-                withdrawal = decimal.Parse(Console.ReadLine());
-            }
+                Console.Write("enter account id: ");
+                int accID = int.Parse(Console.ReadLine());
 
-            matchedAccount.balance -= withdrawal;
-            Console.WriteLine($"withdrew {withdrawal:C} from account {matchedAccount.accountNumber}");
+                var chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+
+                while(chosenAccount.Count() != 1)
+                {
+                    Console.Write("account does not exist. enter another account id: ");
+                    accID = int.Parse(Console.ReadLine());
+
+                    chosenAccount = chosenCustomer.First().accounts.Where(x => x.accountNumber == accID).ToList();
+                }
+
+                matchedAccount = chosenAccount.First();
+                decimal currentBalance = matchedAccount.balance;
+
+                Console.Write("enter withdrawal amount: ");
+                decimal withdrawal = decimal.Parse(Console.ReadLine());
+
+                while(withdrawal <= 0.0m || withdrawal >= (currentBalance * 0.5m))
+                {
+                    Console.Write("withdrawal must be greater than 0.00\nand can not be more than half your current balance.\nenter another withdrawal amount: ");
+                    withdrawal = decimal.Parse(Console.ReadLine());
+                }
+
+                matchedAccount.balance -= withdrawal;
+                Console.WriteLine($"withdrew {withdrawal:C} from account {matchedAccount.accountNumber}");
+            }
         }
 
         private static void ShowCustomers()
@@ -371,6 +463,93 @@ namespace Bank
             Customer pickedCustomer = matchedCustomers.First();
 
             Console.WriteLine(pickedCustomer.PrintAllAccounts());
+        }
+
+        public static void ExpressionTest()
+        {
+            bool toContinue = true;
+            Regex regex = new Regex("");
+
+            while(toContinue)
+            {
+                Console.Write("expression: ");
+                string exp = Console.ReadLine();
+
+                if(exp == "quit")
+                {
+                    toContinue = false;
+                }else
+                {
+                    regex = new Regex(exp);
+
+                    Console.Write("data: ");
+                    string data = Console.ReadLine();
+
+                    Console.WriteLine($"data to be tested:\n{data}");
+
+                    if(regex.IsMatch(data))
+                    {
+                        Console.WriteLine("match");
+                    }
+                    else
+                    {
+                        Console.WriteLine("no match");
+                    }
+                }
+            }                                                 
+        }
+        //Simulate: take the function, split the string to get the cusid,accid and years, validate the customer and account, determine the compound interest and show the user
+        private static void Simulate(string function)
+        {
+            //split the function to get the three variables after simulate keyword
+            string[] simulateCusAccYears = function.Split(" ");
+            int customerID = int.Parse(simulateCusAccYears[1]);
+            int accountID = int.Parse(simulateCusAccYears[2]);
+            int years = int.Parse(simulateCusAccYears[3]);
+
+            //function vars
+            Customer matchedCustomer;
+            Account matchedAccount;
+            double totalValue = 0.0;
+
+            //go through customer list to find the given customer based of ID
+            var customer = customers.Where(x => x.customerID == customerID).ToList();
+
+            if(customer.Count != 1)
+            {
+                Console.WriteLine("this function could not be completed due to a non-existent customer id. please try again");
+            }
+            else
+            {
+                //set matched customer to customer
+                matchedCustomer = customer.First();
+
+                //go through customer accounts to find given account based off ID
+                var account = matchedCustomer.accounts.Where(x => x.accountNumber == accountID).ToList();
+
+                if(account.Count != 1)
+                {
+                    Console.WriteLine("this could not be completed due to a non existent account id. please try again");
+                }
+                else
+                {
+                    //set matched account to account
+                    matchedAccount = account.First();
+
+                    if(matchedAccount.balance <= 0.0m)
+                    {
+                        Console.WriteLine("the balance on this account is too low. please try again");
+                    }
+                    else
+                    {
+                        double dub = 1.0+(((double)matchedAccount.apy)/12);
+
+                        totalValue = (double)matchedAccount.balance * Math.Pow(dub,12*years);
+
+                        Console.WriteLine($"Customer ID: {customerID}\nAccount ID: {accountID}\nStarting Amount: {matchedAccount.balance:C}\nYears: {years}\nInterest Rate: {matchedAccount.apy}\nExpected Value: {totalValue:C}");
+                    }
+                }
+            }
         }
     }
 }
